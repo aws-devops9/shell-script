962b7c8efc03c1c99f8c6762b38b109b08cd1a20
cloudtechprem <cloudtechprem@gmail.com>
2024-03-16 19:26:26 +0530

    1st commit

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "enter your username"
-
 read USERNAME
+echo "enter your password"
+read -s PASSWORD
 
-echo " your username is $USERNAME "
+echo "your username is $USERNAME "
+echo "your password will not be displayed as a security reasons"
