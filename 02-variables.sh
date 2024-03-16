256149df876263b1a8c944fa7506f7d36d3eb23d
cloudtechprem <cloudtechprem@gmail.com>
2024-03-16 13:48:16 +0530

    1st commit

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+echo "This is the comversation between two friends"
+
+echo "Ramesh : Hello Suresh, Good Morning"
+echo "Suresh : Hi Ramesh, very Good Morning"
+echo "Ramesh : How are you? "
+echo "Suresh : I'm Fine, What about you ?"
