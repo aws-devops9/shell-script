9d763264c3c8811ccddc6997a6396f9fc6a12e71
cloudtechprem <cloudtechprem@gmail.com>
2024-03-16 20:05:18 +0530

    4th commit

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "enter your username"
-read -s USERNAME # Here -s used to hide the USERNAME in the command
+read -s USERNAME # Here -s used to hide the USERNAME in the command/terminal
 echo "enter your password"
-read -s PASSWORD # Here -s used to hide the PASSWORD in the command
+read -s PASSWORD # Here -s used to hide the PASSWORD in the command/terminal
 
 echo "your username is $USERNAME"
 echo "your password will not be displayed as a security reasons"
