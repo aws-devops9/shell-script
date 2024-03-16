0fe071265fa54fd6a310bd9b861d60956b128659
cloudtechprem <cloudtechprem@gmail.com>
2024-03-16 13:56:12 +0530

    2nd commit

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,8 +1,11 @@
 #!bin/bash
 
-echo "This is the comversation between two friends"
+echo "This is a comversation between two friends"
 
-echo "Ramesh : Hello Suresh, Good Morning"
-echo "Suresh : Hi Ramesh, very Good Morning"
-echo "Ramesh : How are you? "
-echo "Suresh : I'm Fine, What about you ?"
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1 : Hello $PERSON2, Good Morning"
+echo "$PERSON2 : Hi $PERSON1, very Good Morning"
+echo "$PERSON1 : How are you? "
+echo "$PERSON2 : I'm Fine, What about you ?"
