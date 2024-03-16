f133cf99590a68f7382c3ef4e125b11c10eeb20c
cloudtechprem <cloudtechprem@gmail.com>
2024-03-16 20:22:20 +0530

    1st commit

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -3,9 +3,10 @@
 echo "what is today?"
 read TODAY
 
-if ($TODAY != "Sunday") {
+if ($TODAY !={Sunday}) {
     echo "Today is $TODAY, Go to School"
 }
 else{ 
     echo "Today is $TODAY, Happy Holiday"
 }
+fi
