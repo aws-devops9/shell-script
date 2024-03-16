1491f59ccf8f0531d0c1614c5b8e0cc49159d9c2
cloudtechprem <cloudtechprem@gmail.com>
2024-03-16 18:56:24 +0530

    1st commit

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DATE=$(date) # Here $(date) is the command   
+             # Here date command will give an output and that output will be used as variable for DATE
+
+echo " This script started executing on $DATE. "
+             
