2cf96232cdf006fad1d81bd3b98881312eda7c80
cloudtechprem <cloudtechprem@gmail.com>
2024-03-16 14:04:59 +0530

    4th commit

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,13 @@
 #!bin/bash
 
+# echo "This is a comversation between two friends"
+
+# echo "Ramesh : Hello Suresh, Good Morning"
+# echo "Suresh : Hi Ramesh, very Good Morning"
+# echo "Ramesh : How are you? "
+# echo "Suresh : I'm Fine, What about you ?"
+
+
 echo "This is a comversation between two friends"
 
 PERSON1=Ram
