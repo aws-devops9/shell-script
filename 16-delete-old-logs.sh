58fbc7bfb3327a4e2d519c49eca386dfe8d23aba
cloudtechprem <cloudtechprem@gmail.com>
2024-03-17 19:00:14 +0530

    1st commit

---
+++ 16-delete-old-logs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# we are trying to delete the logs which are older than 14 days
+# example source directory: /tmp/shell-script-logs
+
+R="\e[31m" # Red Colour
+G="\e[32m" # Green colour
+Y="\e[33m" # Yellow colour
+N="\e[0m" # No colour
+
+SOURCE_DIR="/tmp/shell-script-logs"
+
+if [ ! -d $SOURCE_DIR ]
+then
+    echo -e "$R..$SOURCE_DIR $R does not exist $N"
+    exit 1
+fi
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +15 -name "*.log")
+
+while IFS= read -r line # It will read the lines line by line
+do
+    echo "Deleting file: $line"
+    rm -rf $line # It will delete the files line by line
+done <<< $FILES_TO_DELETE #We are giving Files_TO_DeELETE output as an input to while loop here
+
