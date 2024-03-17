a29a2c4ea33ca61403ad0d909826d638c6751c72
cloudtechprem <cloudtechprem@gmail.com>
2024-03-17 19:08:20 +0530

    2nd commit

--- 16-delete-old-logs.sh
+++ 16-delete-old-logs.sh
@@ -8,17 +8,17 @@ G="\e[32m" # Green colour
 Y="\e[33m" # Yellow colour
 N="\e[0m" # No colour
 
-SOURCE_DIR="/tmp/shell-script-logs"
+SOURCE_DIR="/tmp/shell-scipt-logs"
 
 if [ ! -d $SOURCE_DIR ]
 then
-    echo -e "$R..$SOURCE_DIR $R does not exist $N"
+    echo -e "Source Directory $R..$SOURCE_DIR $R does not exist $N"
     exit 1
 fi
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +15 -name "*.log")
 
-while IFS= read -r line # It will read the lines line by line
+while IFS= read -r line # It will read the lines line by line # IFS is Internal Field Separator
 do
     echo "Deleting file: $line"
     rm -rf $line # It will delete the files line by line
