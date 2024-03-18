4432ca7d55f572812717d131a32b4cf2a6f81659
cloudtechprem <cloudtechprem@gmail.com>
2024-03-18 11:20:31 +0530

    2nd commit

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+R="\e[31m" # Red Colour
+G="\e[32m" # Green colour
+Y="\e[33m" # Yellow colour
+N="\e[0m" # No colour
+
 NAME=""
 WISH=""
 
@@ -15,6 +20,18 @@ while getopts ":n:w:h" opt; do
     case $opt in
         n) NAME="$OPTARG";;
         w) WISHES="$OPTARG";;
-        h|*) USAGE; exit;;
+        \?) echo "invalid options: -"$OPTARG"" >&2; USAGE; exit;;
+        :) USAGE; exit;;
+        h) USAGE; exit;;
     esac
 done
+
+if [ -z "$NAME" ] || [ -z "$WISHES" ] # -z means if nothing given for Name and Wishes
+then
+    echo "$R ERROR $N: Both -n and -w are mandotory"
+    USAGE
+    exit 1
+fi 
+
+echo "Hello $NAME. $WISHES. I've been learning DevOps shell script"
+
